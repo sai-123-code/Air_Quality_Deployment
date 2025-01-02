7b26a2ab5b2b7348008d2261427bae4c5d974d60
Jesus Lopez <jesuslopz2204@gmail.com>
2025-01-02 07:18:28 +0000

    interactive filter with the pollutants and messages to population

--- Main_dashboard/Dashboard_pages/page_2.py
+++ Main_dashboard/Dashboard_pages/page_2.py
@@ -1,15 +1,18 @@
 ### Page 2
 
 import streamlit as st
+import numpy as np
 from streamlit_folium import folium_static
 from scripts.map_helpers import create_aqi_heatmap, classical_map, geojson_data
 from scripts.language_utils import get_text
-from scripts.data_handler import get_current_hour_data
+from scripts.data_handler import get_current_hour_data, load_data
 from scripts.data_handler import STATION_COORDINATES
 
 
 def new_home():
     lang = st.session_state.language
+
+    df = load_data()
 
     # Store the initial value of widgets in session state
     if "visibility" not in st.session_state:
@@ -51,147 +54,185 @@ def new_home():
             folium_static(m, width=800, height=700)
     
     with col2:
-        
 
-        st.markdown("""
-                    <div line-height: 0.8;">
-                    <h5>Quality awareness - Health recommendatios</h5>
-                    </div>
-                    """, 
-                    unsafe_allow_html=True)
-        st.markdown("""
-            <style>
-            .bordered-box {
-            border: 1px solid #ffffff;
-            border-radius: 5px;
-            padding: 10px;
-            margin-top: 0px;
-            background-color: #000000;
-            }
-            .bordered-box h4 {
-            margin-top: 0;
-            }
-            </style>
-            """, unsafe_allow_html=True)
+        # Get the markdown for the table and recommendations
+        def get_markdown(df): 
+            st.markdown("""
+                        <div line-height: 0.8;">
+                        <h5>Quality awareness - Health recommendatios</h5>
+                        </div>
+                        """, 
+                        unsafe_allow_html=True)
+            st.markdown("""
+                <style>
+                .bordered-box {
+                border: 1px solid #ffffff;
+                border-radius: 5px;
+                padding: 10px;
+                margin-top: 0px;
+                background-color: #000000;
+                }
+                .bordered-box h4 {
+                margin-top: 0;
+                }
+                </style>
+                """, unsafe_allow_html=True)
 
-        # Content inside the bordered box
-        st.markdown(
-            """
-            <div class="bordered-box" style="font-size: 18px; line-height: 1.2;">
-            <strong>AQI Today:</strong> 139  
-            <span class="green"> Good </span>
-            <br><br>
-            <strong>Población Sensible:</strong> El riesgo en salud es mínimo o nulo. Enjoy outdoor activities
-            <br><br>
-            Población en General: El riesgo en salud es mínimo o nulo. Enjoy outdoor activities
-            <br><br><br><br>
-            <strong>AQI Forecast 24h:</strong> 140
-            <span class="yellow"> Moderate </span> 
-            (<a href="#">Link fore more details</a>)
-            <br><br>
-            <strong>Población Sensible:</strong> Personas que son sensibles al ozono (O3) o material particulado (PM10 y PM2.5). Reduce las actividades físicas vigorosas al aire libre
-            <br><br>
-            Población en General: El riesgo en salud es mínimo. Enjoy outdoor activities
+            # Content inside the bordered box
+            st.markdown(
+                """
+                <div class="bordered-box" style="font-size: 18px; line-height: 1.2;">
+                <strong>AQI Today:</strong> 139  
+                <span class="green"> Good </span>
+                <br><br>
+                <strong>Población Sensible:</strong> El riesgo en salud es mínimo o nulo. Enjoy outdoor activities
+                <br><br>
+                Población en General: El riesgo en salud es mínimo o nulo. Enjoy outdoor activities
+                <br><br><br><br>
+                <strong>AQI Forecast 24h:</strong> 140
+                <span class="yellow"> Moderate </span> 
+                (<a href="#">Link fore more details</a>)
+                <br><br>
+                <strong>Población Sensible:</strong> Personas que son sensibles al ozono (O3) o material particulado (PM10 y PM2.5). Reduce las actividades físicas vigorosas al aire libre
+                <br><br>
+                Población en General: El riesgo en salud es mínimo. Enjoy outdoor activities
+                
+                (<a href="#">click to more details</a>)
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
             
-            (<a href="#">click to more details</a>)
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
-        
-        # Custom CSS for the table
-        #### Need to include the weighted average of the 12h and the average of 24h
-        st.markdown(
-            """
-            <style>
-            .pollutant-table {
-            width: 100%;
-            border-collapse: collapse;
-            background-color: black;
-            color: white;
-            font-size: 10px; /* Reduced base font size */
-            }
-            .pollutant-table th, .pollutant-table td {
-            border: 1px solid white;
-            padding: 5px; /* Reduced padding */
-            text-align: center;
-            }
-            .pollutant-table th {
-            font-weight: bold;
-            background-color: #222;
-            }
-            .pollutant-table td span {
-            font-size: 14px; /* Reduced size for numbers */
-            font-weight: bold;
-            }
-            .green { color: #00FF00; }
-            .yellow { color: #FFD700; }
-            .red { color: #FF4500; }
-            .purple { color: #A020F0; }
-            .orange { color: #FFA500; }
-            </style>
-            """,
-            unsafe_allow_html=True,
-        )
+            # Custom CSS for the table
+            #### Need to include the weighted average of the 12h and the average of 24h
+            st.markdown(
+                """
+                <style>
+                .pollutant-table {
+                width: 100%;
+                border-collapse: collapse;
+                background-color: black;
+                color: white;
+                font-size: 10px; /* Reduced base font size */
+                }
+                .pollutant-table th, .pollutant-table td {
+                border: 1px solid white;
+                padding: 5px; /* Reduced padding */
+                text-align: center;
+                }
+                .pollutant-table th {
+                font-weight: bold;
+                background-color: #222;
+                }
+                .pollutant-table td span {
+                font-size: 14px; /* Reduced size for numbers */
+                font-weight: bold;
+                }
+                .green { color: #00FF00; }
+                .yellow { color: #FFD700; }
+                .red { color: #FF4500; }
+                .purple { color: #A020F0; }
+                .orange { color: #FFA500; }
+                </style>
+                """,
+                unsafe_allow_html=True,
+            )
 
-        st.markdown(""" 
-                    <h5>Table of pollutants </h5> 
-                    """, 
-                    unsafe_allow_html=True)
-        # Table Content
-        st.markdown(
-            """
-            <table class="pollutant-table" style="font-family: Arial, sans-serif; font-size: 12px;">
-            <tr>
-                <th colspan="1">Particulate Matter (µg/m³)</th>
-                <th colspan="1">Actual levels</th>
-                <th colspan="1">Weighted Average last 12h</th>
-                <th colspan="1">Average last 24h</th>
-            </tr>
-            <tr>
-                <td>PM10</td>
-                <td><span class="green">10</span> µg/m³</td>
-                <td> 12 µg/m³</td>
-                <td> 9 µg/m³</td>
-            </tr>
-            <tr>
-                <td>PM2.5</td>
-                <td><span class="yellow">22</span> µg/m³</td>
-                <td> 25 µg/m³</td>
-                <td> 19 µg/m³</td>
-            </tr>
-            <tr>
-                <th colspan="4">Other Pollutants</th>
-            </tr>
-            <tr>
-                <td>Ozone (O₃)</td>
-                <td><span class="red">0.142</span> ppm</td>
-                <td>0.152 ppm</td>
-                <td>0.124 ppm</td>
-            </tr>
-            <tr>
-                <td>Nitrogen dioxide (NO₂)</td>
-                <td><span class="purple">0.222</span> ppm</td>
-                <td>0.253 ppm</td>
-                <td>0.214 ppm</td>
-            </tr>
-            <tr>
-                <td>Sulfur dioxide (SO₂)</td>
-                <td><span class="orange">0.092</span> ppm</td>
-                <td>0.098 ppm</td>
-                <td>0.087 ppm</td>
-            </tr>
-            <tr>
-                <td>Carbon monoxide (CO)</td>
-                <td><span class="red">13.01</span> ppm</td>
-                <td>14.20 ppm</td>
-                <td>12.01 ppm</td>
-            </tr>
-            </table>
-            """,
-            unsafe_allow_html=True,
-        )
-        ##### Need to add as well the AQI from each station, with the string of good, ok, bad...
+            st.markdown(""" 
+                        <h5>Table of pollutants </h5> 
+                        """, 
+                        unsafe_allow_html=True)
+            # Table Content
+            st.markdown(
+                f"""
+                <table class="pollutant-table" style="font-family: Arial, sans-serif; font-size: 12px;">
+                <tr>
+                    <th colspan="1">Particulate Matter (µg/m³)</th>
+                    <th colspan="1">Hourly report</th>
+                    <th colspan="1">Daily report</th>
+                </tr>
+                <tr>
+                    <td>PM10</td>
+                    <td><span class="green"> {pm_list[0]} </span> µg/m³</td>
+                    <td> {pm_list[1]} µg/m³</td>
+                </tr>
+                <tr>
+                    <td>PM2.5</td>
+                    <td><span class="yellow"> {pm_list[2]}</span> µg/m³</td>
+                    <td> {pm_list[3]} µg/m³</td>
+                </tr>
+                <tr>
+                    <th colspan="4">Other Pollutants</th>
+                </tr>
+                <tr>
+                    <td>Ozone (O₃)</td>
+                    <td><span class="red">{pm_list[7]}</span> ppm</td>
+                    <td>{pm_list[7]} ppm</td>
+                </tr>
+                <tr>
+                    <td>Nitrogen dioxide (NO₂)</td>
+                    <td><span class="purple">{pm_list[6]}</span> ppm</td>
+                    <td>{pm_list[6]} ppm</td>
+                </tr>
+                <tr>
+                    <td>Sulfur dioxide (SO₂)</td>
+                    <td><span class="orange">{pm_list[8]}</span> ppm</td>
+                    <td>{pm_list[8]} ppm</td>
+                </tr>
+                <tr>
+                    <td>Carbon monoxide (CO)</td>
+                    <td><span class="red">{pm_list[4]}</span> ppm</td>
+                    <td>{pm_list[5]} ppm</td>
+                </tr>
+                </table>
+                """,
+                unsafe_allow_html=True,
+            )
+            ##### Need to add as well the AQI from each station, with the string of good, ok, bad...
+        
+        
+        # pm10, pm2.5: actual, mean 12 h and 24 h
+        def get_pms(df):
+            # pm10
+            pm_10_12 = int(df.iloc[:11, 4].mean())
+            pm_10_24 = int(df.iloc[:23, 4].mean())
+
+            # pm2.5
+            pm_25_12 = int(df.iloc[:11, 3].mean())
+            pm_25_24 = int(df.iloc[:23, 3].mean())
+
+            # CO
+            CO8h = round(df.iloc[:7, 7].mean(), 2)
+            # Max of 8 hours weighted average of the day
+            COmx8h = round(np.argmax([df.iloc[:8, 3]]), 2)
+
+            # NO2
+            NO2 = round(df.iloc[:, 5].mean(), 3)
+
+            # O3
+            O3 = round(df.iloc[:, 8].mean(), 3)
+
+            # SO2
+            SO2 = round(df.iloc[:, 6].mean(), 3)
+
+
+            pms_list = list([pm_10_12, pm_10_24,
+                            pm_25_12, pm_25_24,
+                            CO8h, COmx8h,
+                            NO2,
+                            O3,
+                            SO2])
+
+            return pms_list
+        
+        pm_list = get_pms(df)
+
+        if selected_zone != None:
+            df = df[df["AQI_STATION"] == selected_zone]
+            pm_list = get_pms(df)
+            get_markdown(df)
+        else:
+            get_markdown(df)
 
 
 ### ====================================================================================================
